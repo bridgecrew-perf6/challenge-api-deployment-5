f432d51760e21b764fa22de42895f011c74dacf4
jotwo <jokotek@gmail.com>
2020-12-11 03:44:47 +0100

    update

--- src/scraping/ImmoWebScraper.py
+++ src/scraping/ImmoWebScraper.py
@@ -14,6 +14,7 @@ import re
 import json
 
 import pandas as pd
+import numpy as np
 
 import pickle
 
@@ -23,12 +24,31 @@ class ImmoWebScraper:
     def __init__(self):
         # The url of each property that resulted from the search will be stored in the "property_url" list.
         self.properties_urls = []
-        self.properties_dicts = []
-        self.properties_df = pd.DataFrame(columns=['price', 'type_property', 'subtype_property', 'area',
-                                                'num_rooms', 'postal_code', 'garden', 'garden_area',
-                                                'terrace', 'terrace_area', 'num_facade', 'building_state',
-                                                'equipped_kitchen', 'furnished', 'open_fire',
-                                                'land_area'])
+        self.properties_df = pd.DataFrame(columns=['ID', 'price', 'type_property', 'subtype_property',
+                                                   'area', 'num_rooms', 'postal_code', 'garden',
+                                                   'garden_area', 'terrace', 'terrace_area', 'num_facades',
+                                                   'building_state', 'equipped_kitchen', 'furnished',
+                                                   'open_fire', 'swimming_pool', 'land_area'])
+        self.properties_df_dtypes = {
+            'ID':               int,
+            'price':            float,
+            'type_property':    str,
+            'subtype_property': str,
+            'area':             float,
+            'num_rooms':        float,
+            'postal_code':      float,
+            'garden':           float,
+            'garden_area':      float,
+            'terrace':          float,
+            'terrace_area':     float,
+            'num_facades':      float,
+            'building_state':   str,
+            'equipped_kitchen': float,
+            'furnished':        float,
+            'open_fire':        float,
+            'swimming_pool':    float,
+            'land_area':        float
+        }
     
     def get_properties_df(self) -> pd.DataFrame:
         return self.properties_df.copy(deep=True)
@@ -47,7 +67,7 @@ class ImmoWebScraper:
         # Iterate through all result pages (i) and get the url of each of them
         base_url = 'https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&isALifeAnnuitySale=false&isAnInvestmentProperty=false&isAPublicSale=false&propertySubtypes=BUNGALOW,CHALET,FARMHOUSE,EXCEPTIONAL_PROPERTY,COUNTRY_COTTAGE,CASTLE,TOWN_HOUSE,MANSION,VILLA,MANOR_HOUSE,GROUND_FLOOR,DUPLEX,FLAT_STUDIO,LOFT,KOT,PENTHOUSE,TRIPLEX'
         
-        num_pages = 1
+        num_pages = 350
 
         for i in range(1, num_pages+1):
             page_num_suffix = '&page=' + str(i)
@@ -72,8 +92,8 @@ class ImmoWebScraper:
             self.properties_urls.append(page_properties_url)
 
             # An implicit wait tells WebDriver to poll the DOM for a
-            #  certain amount of time when trying to find any element 
-            #     (or elements) not immediately available. 
+            # certain amount of time when trying to find any element 
+            # (or elements) not immediately available. 
             driver.implicitly_wait(10)
 
         # Store all houses urls in a csv file
@@ -88,11 +108,15 @@ class ImmoWebScraper:
         driver.close()
 
     def scrape_all_properties_data(self) -> None:
-        self.properties_dicts = [{}]*len(self.properties_urls)
+        properties_dicts = [{}]*len(self.properties_urls)
         for i, property_url in enumerate(self.properties_urls):
-            property_dict = ImmoWebScraper.scrape_unique_property_data(property_url)
-            self.properties_dicts[i] = property_dict
-            self.properties_df = self.properties_df.append(property_dict, ignore_index=True)
+            print(i) 
+            properties_dicts[i] = ImmoWebScraper.scrape_unique_property_data(property_url)
+
+        self.properties_df = pd.DataFrame(properties_dicts)
+
+        for col_name, dtype in self.properties_df_dtypes.items():
+            self.properties_df[col_name] = self.properties_df[col_name].astype(dtype)
     
     @staticmethod
     def scrape_unique_property_data(url: str) -> dict:
@@ -103,6 +127,7 @@ class ImmoWebScraper:
         final_property_dict = {}
         
         # set of attributes collected in the dictionary
+        final_property_dict['ID'] = ImmoWebScraper.ID(property_dict)
         final_property_dict['price'] = ImmoWebScraper.price(property_dict)
         final_property_dict['type_property'] = ImmoWebScraper.type_property(property_dict)
         final_property_dict['subtype_property'] = ImmoWebScraper.subtype_property(property_dict)
@@ -113,11 +138,12 @@ class ImmoWebScraper:
         final_property_dict['garden_area'] = ImmoWebScraper.garden_area(property_dict)
         final_property_dict['terrace'] = ImmoWebScraper.terrace(property_dict)
         final_property_dict['terrace_area'] = ImmoWebScraper.terrace_area(property_dict)
-        final_property_dict['num_facade'] = ImmoWebScraper.num_facade(property_dict)
+        final_property_dict['num_facades'] = ImmoWebScraper.num_facades(property_dict)
         final_property_dict['building_state'] = ImmoWebScraper.building_state(property_dict)
         final_property_dict['equipped_kitchen'] = ImmoWebScraper.equipped_kitchen(property_dict)
         final_property_dict['furnished'] = ImmoWebScraper.furnished(property_dict)
         final_property_dict['open_fire'] = ImmoWebScraper.open_fire(property_dict)
+        final_property_dict['swimming_pool'] = ImmoWebScraper.swimming_pool(property_dict)
         final_property_dict['land_area'] = ImmoWebScraper.land_area(property_dict)
 
         return final_property_dict
@@ -153,10 +179,15 @@ class ImmoWebScraper:
             property_dict = json.loads(wcs)
 
             return property_dict
+
         except:
             return None
     
-    # Define a method to scrap each property attribute    
+    # Define a method to scrap each property attribute
+    @staticmethod
+    def ID(property_dict: dict) -> int:
+        return int(property_dict['id'])
+
     @staticmethod
     def price(property_dict: dict) -> int:
         try:
@@ -202,7 +233,7 @@ class ImmoWebScraper:
     @staticmethod
     def garden(property_dict: dict) -> int:
         try:
-            if property_dict['property']['hasGarden'] ==  True:
+            if property_dict['property']['hasGarden']:
                 return 1
             else:
                 return 0
@@ -212,17 +243,14 @@ class ImmoWebScraper:
     @staticmethod
     def garden_area(property_dict: dict) -> int:
         try:
-            if property_dict['property']['hasGarden'] ==  True:
-                return property_dict['property']['gardenSurface']
-            else:
-                return 0
+            return property_dict['property']['gardenSurface']
         except:
             return None
     
     @staticmethod
     def terrace(property_dict: dict) -> int:
         try:
-            if property_dict['property']['hasTerrace'] == True:
+            if property_dict['property']['hasTerrace']:
                 return 1
             else:
                 return 0
@@ -232,15 +260,12 @@ class ImmoWebScraper:
     @staticmethod
     def terrace_area(property_dict: dict) -> int:
         try:
-            if property_dict['property']['hasTerrace'] == True:
-                return int(property_dict['property']['terraceSurface'])
-            else:
-                return 0
+            return int(property_dict['property']['terraceSurface'])
         except:
             return None
     
     @staticmethod
-    def num_facade(property_dict: dict) -> int:
+    def num_facades(property_dict: dict) -> int:
         try:
             return int(property_dict['property']['building']['facadeCount'])
         except:
@@ -249,22 +274,32 @@ class ImmoWebScraper:
     @staticmethod
     def building_state(property_dict: dict) -> str: 
         try:
-            return property_dict['property']['building']['condition']
+            state = property_dict['property']['building']['condition']
         except:
-            return None
+            state = None
+
+        try:
+            state = 'NEW' if property_dict['flags']['isNewlyBuilt'] == True else state
+        except:
+            pass
+
+        return state
     
     @staticmethod
     def equipped_kitchen(property_dict: dict) -> int:
         try: 
-            property_dict['property']['kitchen']['type']
-            return 1
+            kitchen_type = property_dict['property']['kitchen']['type']
+            if kitchen_type == 'NOT_INSTALLED':
+                return 0
+            else:
+                return 1
         except:
-            return 0
+            return None
     
     @staticmethod
     def furnished(property_dict: dict) -> int:
         try:
-            if property_dict['transaction']['sale']['isFurnished'] == True:
+            if property_dict['transaction']['sale']['isFurnished']:
                 return 1
             else:
                 return 0
@@ -274,8 +309,17 @@ class ImmoWebScraper:
     @staticmethod    
     def open_fire(property_dict: dict) -> int:
         try:
-            fire = property_dict['property']['fireplaceExists']
-            if fire == True:
+            if property_dict['property']['fireplaceExists']:
+                return 1 
+            else:
+                return 0                
+        except:
+            return None
+    
+    @staticmethod    
+    def swimming_pool(property_dict: dict) -> int:
+        try:
+            if property_dict['property']['hasSwimmingPool']:
                 return 1 
             else:
                 return 0                
@@ -288,21 +332,66 @@ class ImmoWebScraper:
             if property_dict['property']['land'] != None:
                 return int(property_dict['property']['land']['surface'])
             else:
-                return 0
+                return None
         except:
             return None
+    
+
+    @staticmethod
+    def to_region(postal_code: float) -> str:
+        if pd.isna(postal_code):
+            region = None
+        else:
+            #casting: 'float' -> 'int'
+            postal_code = int(postal_code)
+            #'B' -> Brussels-Capital Region
+            #'W' -> Walloon Region
+            #'F' -> Flemish Region
+            if 1000 <= postal_code and postal_code <= 1299:
+                region = 'B'
+            elif (1300 <= postal_code and postal_code <= 1499) or (4000 <= postal_code and postal_code <= 7999):
+                region = 'W'
+            else:
+                region = 'F'
+        return region
+    
+    @staticmethod
+    def recategorize_state(category: str) -> str:
+        new_category = category
+        if category in ['TO_RESTORE', 'TO_RENOVATE', 'TO_BE_DONE_UP']:
+            new_category = 'to_renovate'
+        elif category in ['GOOD', 'AS_NEW']:
+            new_category = 'good'
+        elif category == 'JUST_RENOVATED':
+            new_category = 'renovated'
+        elif category == 'NEW':
+            new_category = 'new'
+        return new_category
+    
+    def drop_duplicates_and_set_index(self) -> None:
+        self.properties_df.drop_duplicates(subset=['ID'], inplace=True)
+        self.properties_df.set_index('ID', inplace=True)
+    
+    def remove_wrongly_scraped_data(self) -> None:
+        self.properties_df = self.properties_df[(self.properties_df['type_property'] == 'APARTMENT') 
+                                                | (self.properties_df['type_property'] == 'HOUSE')]
+        self.properties_df.dropna(subset=['price'], inplace=True)
+    
+    def scrape_and_arrange_data(self) -> None:
+        self.scrape_properties_urls()
+        self.scrape_all_properties_data()
+        self.drop_duplicates_and_set_index()
+        self.remove_wrongly_scraped_data()
+        self.properties_df['building_state'] = self.properties_df['building_state'].apply(ImmoWebScraper.recategorize_state)
+        self.properties_df['region'] = self.properties_df['postal_code'].map(ImmoWebScraper.to_region)
 
 
 if __name__ == '__main__':
     scraper = ImmoWebScraper()
-            
-    scraper.scrape_properties_urls()
-
-    scraper.scrape_all_properties_data()
+    scraper.scrape_and_arrange_data()
 
     print(scraper.properties_df)
 
-    # for col in scraper.properties_df.columns:
-    #     print(scraper.properties_df[col])
+    print(scraper.properties_df.dtypes)
 
     scraper.pickle_properties_df()
